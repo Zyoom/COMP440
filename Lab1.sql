0bd0d6c1e548c5c250acc4dd09f4f6423be6c8dc
David Pedroza <david.pedroza.662@my.csun.edu>
2024-01-30 22:43:44 -0800

    Completed First Lab In COMP440

---
+++ Lab1.sql
@@ -0,0 +1,9 @@
+use comp440;
+create table Movie(
+	ID smallInt unsigned,
+    Title varchar(50),
+    Rating char(4),
+    ReleaseDate date,
+    Budget decimal(8,2)
+    );
+    Select * from Movie
